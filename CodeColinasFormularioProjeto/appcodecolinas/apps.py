e7e37151bed33ba3244096aee484ebee52da9b19
Lenon Yuri <lenonyurisilva26@gmail.com>
2023-09-19 12:18:19 -0300

    19/09/23

---
+++ CodeColinasFormularioProjeto/appcodecolinas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppcodecolinasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'appcodecolinas'
---
+++ CodeColinasFormularioProjeto/appcodecolinas/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+def cadastrocliente(request):
+    return render(request, 'cadastrocliente.html')
+
+def processarcadastro(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        # ... outros campos ...
+        mensagem = request.POST.get('mensagem')
+        # Faça o que for necessário com os dados
+
+        # Por exemplo, você pode salvar os dados em um banco de dados:
+        # Cliente.objects.create(nome=nome, email=email, ...)
+
+        # E então redirecionar para uma página de confirmação ou outra página
+        return render(request, 'confirmacaocadastro.html')
