47a7a430991794e8566e6ef97121075af5a8835d
Lenon Yuri <lenonyurisilva26@gmail.com>
2023-09-28 12:29:50 -0300

    28/09/23

--- ProjetoCodeColinas/projetocodecolinas/codecolinasapp/views.py
+++ ProjetoCodeColinas/projetocodecolinas/codecolinasapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import PedidoForm, ProdutoForm, CategoriaForm
 from .models import Produto, Categoria
+
+
 def lista_produtos(request):
     produtos = Produto.objects.all()
     categorias = Categoria.objects.all()
@@ -10,8 +12,9 @@ def lista_produtos(request):
         if produto_form.is_valid():
             produto_form.save()
             return redirect('lista_produtos')
-    return render(request, 'lista_produtos.html', {'produtos': produtos, 'categorias': categorias, 
-                                                   'produto_form': produto_form, 'categoria_form': categoria_form})
+    return render(request, 'lista_produtos.html', {'produtos': produtos, 'categorias': categorias, 'produto_form': produto_form, 'categoria_form': categoria_form})
+
+
 def criar_pedido(request):
     form = PedidoForm(request.POST or None)
 
@@ -20,6 +23,8 @@ def criar_pedido(request):
             form.save()
             return redirect('lista_produtos')
     return render(request, 'criar_pedido.html', {'form': form})
+
+
 def criar_produto(request):
     if request.method == 'POST':
         form = ProdutoForm(request.POST)
@@ -27,6 +32,8 @@ def criar_produto(request):
             form.save()
             return redirect('lista_produtos')
     return redirect('lista_produtos')
+
+
 def inserir_categoria(request):
     form = CategoriaForm(request.POST or None)
     if request.method == 'POST':
