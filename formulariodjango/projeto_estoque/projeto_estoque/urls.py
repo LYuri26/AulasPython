29e2e02790c002ad066a2513fb738e986c2d8460
Lenon Yuri <lenonyurisilva26@gmail.com>
2023-10-05 03:03:02 -0300

    05/10/23

--- formulariodjango/projeto_estoque/projeto_estoque/urls.py
+++ formulariodjango/projeto_estoque/projeto_estoque/urls.py
@@ -14,15 +14,42 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Importa a classe `admin` do módulo `django.contrib`
 from django.contrib import admin
-from django.urls import path, include
-from .views import lista_estoque, adicionar_item, editar_item
 
+# Importa a função `path` e o módulo `include` do módulo `django.urls`
+from django.urls import path, include
+
+# Importa as views do aplicativo `estoque`
+from estoque import views
+
+
+# Lista de URLs
 urlpatterns = [
-    path('', lista_estoque, name='lista_estoque'),
-    path('admin/', admin.site.urls),
-    path('estoque/', include('estoque.urls')),
-    path('adicionar/', adicionar_item, name='adicionar_item'),
-    path('editar/<int:id>/', editar_item, name='editar_item'),
-    # ...
+    # Define a rota para o painel de administração do Django
+    path("admin/", admin.site.urls),
+    
+    # Define uma rota para a criação de um novo item
+    path("novo_item/", views.novo_item, name="novo_item"),
+    
+    # Define uma rota para a adição de um novo item
+    path("adicionar/", views.adicionar_item, name="adicionar_item"),
+    
+    # Define a rota padrão, que lista os itens em estoque
+    path("", views.lista_estoque),
+    
+    # Define uma rota para listar os itens em estoque
+    path("lista_estoque/", views.lista_estoque, name="lista_estoque"),
+    
+    # Define uma rota para os detalhes de um item
+    path("<int:id>/", views.detalhes_item, name="detalhes_item"),
+    
+    # Define uma rota para editar um item
+    path("<int:id>/editar/", views.editar_item, name="editar_item"),
+    
+    # Define uma rota para confirmar a exclusão de um item
+    path("<int:id>/excluir/",views.confirmar_exclusao_item, name="confirmar_exclusao_item"),
+    
+    # Define uma rota para efetuar a exclusão de um item
+    path('<int:id>/excluir/confirmar/', views.excluir_item, name='excluir_item'),
 ]
